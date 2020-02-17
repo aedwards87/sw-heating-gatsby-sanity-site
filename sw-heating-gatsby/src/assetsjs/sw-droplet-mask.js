28c274f7ce8c1d840d3390754d10bc3be8144313
Adam Edwards <aedwards1987@gmail.com>
2020-02-17 13:19:30 +0000

    commute

--- sw-heating-gatsby/src/assetsjs/sw-droplet-mask.js
+++ sw-heating-gatsby/src/assetsjs/sw-droplet-mask.js
@@ -1,11 +1,11 @@
 import React, { useContext, useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { UserContext } from '../components/index'
-import { useTransition, animated, config } from 'react-spring'
+import { useTransition, animated } from 'react-spring'
 
 
 const pages = [
-  ({ style, navBarHeight, className, height }) => (
+  ({ style, navBarHeight, className, height, click }) => (
     <StyledMaskContainer navBarHeight={navBarHeight} style={style} >
       <svg height={height} className={className, 'SVG'} viewBox="0 0 476 588" fill="none"
         style={{ filter: 'drop-shadow(40px 55px 40px rgba(0, 0, 0, .7))' }}
@@ -17,12 +17,12 @@ const pages = [
         </defs>
         <StyledImage xlinkHref={require("../images/image1.jpg")} alt={'To come'} className="clip-svg" />
       </svg>
-      <StyledDropletBorderSVG height={height} className={className} viewBox="0 0 476 588" fill="none">
+      <StyledDropletBorderSVG onClick={click} height={height} className={className} viewBox="0 0 476 588" fill="none">
         <path d="M24.4264 443.301C-6.4996 365.792 12.5404 278.09 79.4318 209.142C136.46 152.067 175.028 112.456 200.862 81.4301C217.778 61.1149 229.517 44.1561 237.482 28.0274C245.835 44.9491 258.302 62.4026 275.827 83.0359C300.917 112.578 337.174 149.53 388.148 201.481C390.615 203.994 393.116 206.543 395.651 209.128L395.684 209.161L395.716 209.193C462.65 276.182 483.556 365.859 450.706 443.216L450.632 443.391L450.565 443.568C419.591 524.939 338.001 578 239.588 578C141.319 578 59.4781 525.028 24.4264 443.301Z" stroke="white" strokeWidth="20" />
       </StyledDropletBorderSVG>
     </StyledMaskContainer>
   ),
-  ({ style, navBarHeight, className, height }) => (
+  ({ style, navBarHeight, className, height, click }) => (
     <StyledMaskContainer navBarHeight={navBarHeight} style={style} >
       <svg height={height} className={className, 'SVG'} viewBox="0 0 476 588" fill="none"
         style={{ filter: 'drop-shadow(40px 55px 40px rgba(0, 0, 0, .7))' }}
@@ -34,13 +34,13 @@ const pages = [
         </defs>
         <StyledImage xlinkHref={require("../images/image1.jpg")} alt={'To come'} className="clip-svg" />
       </svg>
-      <StyledDropletBorderSVG height={height} className={className} viewBox="0 0 476 588" fill="none">
+      <StyledDropletBorderSVG onClick={click} height={height} className={className} viewBox="0 0 476 588" fill="none">
         <path d="M24.4264 443.301C-6.4996 365.792 12.5404 278.09 79.4318 209.142C136.46 152.067 175.028 112.456 200.862 81.4301C217.778 61.1149 229.517 44.1561 237.482 28.0274C245.835 44.9491 258.302 62.4026 275.827 83.0359C300.917 112.578 337.174 149.53 388.148 201.481C390.615 203.994 393.116 206.543 395.651 209.128L395.684 209.161L395.716 209.193C462.65 276.182 483.556 365.859 450.706 443.216L450.632 443.391L450.565 443.568C419.591 524.939 338.001 578 239.588 578C141.319 578 59.4781 525.028 24.4264 443.301Z" stroke="white" strokeWidth="20" />
       </StyledDropletBorderSVG>
     </StyledMaskContainer>
   ),
-  ({ style, navBarHeight, className, height }) => (
-    <StyledMaskContainer navBarHeight={navBarHeight} style={style} >
+  ({ style, navBarHeight, className, height, click }) => (
+    <StyledMaskContainer navBarHeight={navBarHeight} style={style}>
       <svg height={height} className={className, 'SVG'} viewBox="0 0 476 588" fill="none"
         style={{ filter: 'drop-shadow(40px 55px 40px rgba(0, 0, 0, .7))' }}
       >
@@ -51,12 +51,12 @@ const pages = [
         </defs>
         <StyledImage xlinkHref={require("../images/image1.jpg")} alt={'To come'} className="clip-svg" />
       </svg>
-      <StyledDropletBorderSVG height={height} className={className} viewBox="0 0 476 588" fill="none">
+      <StyledDropletBorderSVG onClick={click} height={height} className={className} viewBox="0 0 476 588" fill="none">
         <path d="M24.4264 443.301C-6.4996 365.792 12.5404 278.09 79.4318 209.142C136.46 152.067 175.028 112.456 200.862 81.4301C217.778 61.1149 229.517 44.1561 237.482 28.0274C245.835 44.9491 258.302 62.4026 275.827 83.0359C300.917 112.578 337.174 149.53 388.148 201.481C390.615 203.994 393.116 206.543 395.651 209.128L395.684 209.161L395.716 209.193C462.65 276.182 483.556 365.859 450.706 443.216L450.632 443.391L450.565 443.568C419.591 524.939 338.001 578 239.588 578C141.319 578 59.4781 525.028 24.4264 443.301Z" stroke="white" strokeWidth="20" />
       </StyledDropletBorderSVG>
     </StyledMaskContainer>
   ),
-  ({ style, navBarHeight, className, height }) => (
+  ({ style, navBarHeight, className, height, click }) => (
     <StyledMaskContainer navBarHeight={navBarHeight} style={style} >
       <svg height={height} className={className, 'SVG'} viewBox="0 0 476 588" fill="none"
         style={{ filter: 'drop-shadow(40px 55px 40px rgba(0, 0, 0, .7))' }}
@@ -68,13 +68,13 @@ const pages = [
         </defs>
         <StyledImage xlinkHref={require("../images/image1.jpg")} alt={'To come'} className="clip-svg" />
       </svg>
-      <StyledDropletBorderSVG height={height} className={className} viewBox="0 0 476 588" fill="none">
+      <StyledDropletBorderSVG onClick={click} height={height} className={className} viewBox="0 0 476 588" fill="none">
         <path d="M24.4264 443.301C-6.4996 365.792 12.5404 278.09 79.4318 209.142C136.46 152.067 175.028 112.456 200.862 81.4301C217.778 61.1149 229.517 44.1561 237.482 28.0274C245.835 44.9491 258.302 62.4026 275.827 83.0359C300.917 112.578 337.174 149.53 388.148 201.481C390.615 203.994 393.116 206.543 395.651 209.128L395.684 209.161L395.716 209.193C462.65 276.182 483.556 365.859 450.706 443.216L450.632 443.391L450.565 443.568C419.591 524.939 338.001 578 239.588 578C141.319 578 59.4781 525.028 24.4264 443.301Z" stroke="white" strokeWidth="20" />
       </StyledDropletBorderSVG>
     </StyledMaskContainer>
   ),
-  ({ style, navBarHeight, className, height }) => (
-    <StyledMaskContainer navBarHeight={navBarHeight} style={style} >
+  ({ style, navBarHeight, className, height, click }) => (
+    <StyledMaskContainer navBarHeight={navBarHeight} style={style}>
       <svg height={height} className={className, 'SVG'} viewBox="0 0 476 588" fill="none"
         style={{ filter: 'drop-shadow(40px 55px 40px rgba(0, 0, 0, .7))' }}
       >
@@ -85,7 +85,7 @@ const pages = [
         </defs>
         <StyledImage xlinkHref={require("../images/image1.jpg")} alt={'To come'} className="clip-svg" />
       </svg>
-      <StyledDropletBorderSVG height={height} className={className} viewBox="0 0 476 588" fill="none">
+      <StyledDropletBorderSVG onClick={click} height={height} className={className} viewBox="0 0 476 588" fill="none">
         <path d="M24.4264 443.301C-6.4996 365.792 12.5404 278.09 79.4318 209.142C136.46 152.067 175.028 112.456 200.862 81.4301C217.778 61.1149 229.517 44.1561 237.482 28.0274C245.835 44.9491 258.302 62.4026 275.827 83.0359C300.917 112.578 337.174 149.53 388.148 201.481C390.615 203.994 393.116 206.543 395.651 209.128L395.684 209.161L395.716 209.193C462.65 276.182 483.556 365.859 450.706 443.216L450.632 443.391L450.565 443.568C419.591 524.939 338.001 578 239.588 578C141.319 578 59.4781 525.028 24.4264 443.301Z" stroke="white" strokeWidth="20" />
       </StyledDropletBorderSVG>
     </StyledMaskContainer>
@@ -107,40 +107,63 @@ const DropletImageMask = ({ height = '80vh', className }) => {
     config: { mass: 11, tension: 300, friction: 120 }
   })
 
-  // const Timer = useEffect(() => setInterval(() => setIndex(state => (state + 1) % 5), 6000), [])
+  useEffect(() => {
+    const Timer = setInterval(() => {
+      setIndex(state => (state + 1) % 5)
+    }, 4000)
+    return () => clearInterval(Timer)
+  })
 
-  const stopTimer = clearInterval()
+
 
 
 
   return (
-    <StyledDropletImageMaskContainer navBarHeight={navBarHeight} onClick={onClick}>
+    <StyledDropletImageMaskContainer navBarHeight={navBarHeight} >
 
       {transitions.map(({ item, props, key }) => {
         const Page = pages[item]
-        return <Page key={key} style={props} height={height} className={className} navBarHeight={navBarHeight} />
+        return <Page key={key} click={onClick} style={props} height={height} className={className} navBarHeight={navBarHeight} />
       })}
-      <button style={{ bottom: 0, position: 'absolute' }}>STOP</button>
-      {/* <svg height={height} className={className, 'SVG'} viewBox="0 0 476 588" fill="none"
-          style={{ filter: 'drop-shadow(40px 55px 40px rgba(0, 0, 0, .7))' }}
-        >
-          <defs>
-            <clipPath id="myClip">
-              <path d="M239.588 588C137.587 588 51.9062 532.875 15.1858 447.125C-17.4546 365.458 2.94566 273.583 72.3064 202.125C186.548 87.7917 225.308 44.9167 237.548 0C249.788 46.9583 292.629 89.8333 402.79 202.125C472.151 271.542 494.591 365.458 459.91 447.125C427.27 532.875 341.589 588 239.588 588Z" fill="black" />
-            </clipPath>
-          </defs>
-          <StyledImage xlinkHref={require("../images/image1.jpg")} alt={'To come'} className="clip-svg" />
-        </svg>
-
-        <StyledDropletBorderSVG height={height} className={className} viewBox="0 0 476 588" fill="none">
-          <path d="M24.4264 443.301C-6.4996 365.792 12.5404 278.09 79.4318 209.142C136.46 152.067 175.028 112.456 200.862 81.4301C217.778 61.1149 229.517 44.1561 237.482 28.0274C245.835 44.9491 258.302 62.4026 275.827 83.0359C300.917 112.578 337.174 149.53 388.148 201.481C390.615 203.994 393.116 206.543 395.651 209.128L395.684 209.161L395.716 209.193C462.65 276.182 483.556 365.859 450.706 443.216L450.632 443.391L450.565 443.568C419.591 524.939 338.001 578 239.588 578C141.319 578 59.4781 525.028 24.4264 443.301Z" stroke="white" strokeWidth="20" />
-        </StyledDropletBorderSVG> */}
+      <ButtonssContainer>
+        <Button onClick={onClick} className={index === 0 && 'active'} />
+        <Button onClick={onClick} className={index === 1 && 'active'} />
+        <Button onClick={onClick} className={index === 2 && 'active'} />
+        <Button onClick={onClick} className={index === 3 && 'active'} />
+        <Button onClick={onClick} className={index === 4 && 'active'} />
+        <label><input type="radio" /></label>
+      </ButtonssContainer>
 
     </StyledDropletImageMaskContainer>
   )
 }
 
 const AnimMaskContainer = animated.div
+
+const ButtonssContainer = styled.div`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  z-index: 99999;
+  input {
+    border: 2px solid #000000;
+    background: transparent;
+  }
+`
+
+const Button = styled.button`
+  border-radius: 100%;
+  height: 10px;
+  width: 10px;
+  border: 2px solid #000000;
+  background: transparent;
+  margin: 3px;
+  cursor: pointer;
+  :hover,
+  &.active {
+    background: #000000;
+  }
+`
 
 const StyledDropletImageMaskContainer = styled.div`
   position: absolute;
