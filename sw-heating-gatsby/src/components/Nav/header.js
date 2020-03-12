48b31a18f6b5acf6ceaa1f02fc7d82cc7583e41f
Adam Edwards <aedwards1987@gmail.com>
2020-03-12 19:24:16 +0000

    commute

--- sw-heating-gatsby/src/components/Nav/header.js
+++ sw-heating-gatsby/src/components/Nav/header.js
@@ -18,66 +18,66 @@ const Header = (props, ref) => {
         <Toggle>
           {({ on: isDropDownOpen, toggleOff, toggleOn }) =>
             <Toggle>
-            {({ on: isMenuOpen, toggle: toggleMenu }) =>
-              
-            <S.Header
-              id="header"
-              scrollUp={scrollUp ? true : false}
-              position={currentPosition}
-              ref={ref}
-              isMenuOpen={isMenuOpen}
-            >
-              
-              <div>
-                <Link to="/">
-                  <SWHeatingLogo height="49" />
-                </Link>
-                <Desktop>
-                  <NavBar
-                    on={isDropDownOpen}
-                    ToggleOn={toggleOn}
-                    ToggleOff={toggleOff}
-                    location={location}
-                    navLinks={navLinks}
-                  />
-                </Desktop>
-                <SmallScreen>
-                  {/* <Toggle>
+              {({ on: isMenuOpen, toggle: toggleMenu }) =>
+
+                <S.Header
+                  id="header"
+                  scrollUp={scrollUp ? true : false}
+                  position={currentPosition}
+                  ref={ref}
+                  isMenuOpen={isMenuOpen}
+                >
+
+                  <div>
+                    <Link to="/">
+                      <SWHeatingLogo height="49" />
+                    </Link>
+                    <Desktop>
+                      <NavBar
+                        on={isDropDownOpen}
+                        ToggleOn={toggleOn}
+                        ToggleOff={toggleOff}
+                        location={location}
+                        navLinks={navLinks}
+                      />
+                    </Desktop>
+                    <SmallScreen>
+                      {/* <Toggle>
                     {({ on: isMenuOpen, toggle: toggleMenu }) => */}
                       {/* <> */}
-                        <S.MenuButton 
-                          className={`hamburger hamburger--collapse is-active`}
-                          type="button" 
-                          onClick={() => {
-                            toggleMenu()
-                            setScrollUp(true)
-                          }}
-                        >
-                          {/* <MenuIcon /> */}
-                          {/* <button class="hamburger hamburger--collapse" type="button"> */}
-                            <span class="hamburger-box">
-                              <span class="hamburger-inner" />
-                            </span>
-                          {/* </button> */}
-                        </S.MenuButton>
-                        <NavMenu
-                          isMenuOpen={isMenuOpen}
-                          toggleMenu={toggleMenu}
-                          location={location}
-                          navLinks={navLinks}
-                          scrollUp={scrollUp}
-                        />
+                      <S.MenuButton
+                        className={`hamburger hamburger--collapse ${isMenuOpen ? 'is-active' : null}`}
+                        type="button"
+                        onClick={() => {
+                          toggleMenu()
+                          setScrollUp(true)
+                        }}
+                      >
+                        {/* <MenuIcon /> */}
+                        {/* <button class="hamburger hamburger--collapse" type="button"> */}
+                        <span class="hamburger-box">
+                          <span class="hamburger-inner" />
+                        </span>
+                        {/* </button> */}
+                      </S.MenuButton>
+                      <NavMenu
+                        isMenuOpen={isMenuOpen}
+                        toggleMenu={toggleMenu}
+                        location={location}
+                        navLinks={navLinks}
+                        scrollUp={scrollUp}
+                      />
                       {/* </> */}
-                    
-                </SmallScreen>
-              </div>
-            </S.Header >
-            }
+
+                    </SmallScreen>
+                  </div>
+                </S.Header >
+              }
             </Toggle>
           }
         </Toggle>
       }
-    </Location>
+    </Location >
   )
 }
 
@@ -105,55 +105,55 @@ const S = {
     border: none;
     background: none;
     cursor: pointer;
-    .hamburger {
-  padding: 15px 15px;
-  display: inline-block;
-  cursor: pointer;
-  transition-property: opacity, filter;
-  transition-duration: 0.15s;
-  transition-timing-function: linear;
-  font: inherit;
-  color: inherit;
-  text-transform: none;
-  background-color: transparent;
-  border: 0;
-  margin: 0;
-  overflow: visible; }
-  .hamburger:hover {
-    opacity: 0.7; }
-  .hamburger.is-active:hover {
-    opacity: 0.7; }
-  .hamburger.is-active .hamburger-inner,
-  .hamburger.is-active .hamburger-inner::before,
-  .hamburger.is-active .hamburger-inner::after {
-    background-color: #000; }
-
-.hamburger-box {
-  width: 40px;
-  height: 24px;
-  display: inline-block;
-  position: relative; }
-
-.hamburger-inner {
-  display: block;
-  top: 50%;
-  margin-top: -2px; }
-  .hamburger-inner, .hamburger-inner::before, .hamburger-inner::after {
-    width: 40px;
-    height: 4px;
-    background-color: #000;
-    border-radius: 4px;
-    position: absolute;
-    transition-property: transform;
+    &.hamburger {
+    padding: 15px 15px;
+    display: inline-block;
+    cursor: pointer;
+    transition-property: opacity, filter;
     transition-duration: 0.15s;
-    transition-timing-function: ease; }
-  .hamburger-inner::before, .hamburger-inner::after {
-    content: "";
-    display: block; }
-  .hamburger-inner::before {
-    top: -10px; }
-  .hamburger-inner::after {
-    bottom: -10px; }
+    transition-timing-function: linear;
+    font: inherit;
+    color: inherit;
+    text-transform: none;
+    background-color: transparent;
+    border: 0;
+    margin: 0;
+    overflow: visible; }
+    .hamburger:hover {
+      opacity: 0.7; }
+    .hamburger.is-active:hover {
+      opacity: 0.7; }
+    .hamburger.is-active &.hamburger-inner,
+    .hamburger.is-active &.hamburger-inner::before,
+    .hamburger.is-active &.hamburger-inner::after {
+      background-color: #000; }
+
+  .hamburger-box {
+    width: 40px;
+    height: 24px;
+    display: inline-block;
+    position: relative; }
+
+  .hamburger-inner {
+    display: block;
+    top: 50%;
+    margin-top: -2px; }
+    .hamburger-inner, .hamburger-inner::before, .hamburger-inner::after {
+      width: 40px;
+      height: 4px;
+      background-color: #000;
+      border-radius: 4px;
+      position: absolute;
+      transition-property: transform;
+      transition-duration: 0.15s;
+      transition-timing-function: ease; }
+    .hamburger-inner::before, .hamburger-inner::after {
+      content: "";
+      display: block; }
+    .hamburger-inner::before {
+      top: -10px; }
+    .hamburger-inner::after {
+      bottom: -10px; }
     .hamburger--collapse .hamburger-inner {
       top: auto;
       bottom: 0;
@@ -168,17 +168,17 @@ const S = {
     .hamburger--collapse .hamburger-inner::before {
       transition: top 0.12s 0.2s cubic-bezier(0.33333, 0.66667, 0.66667, 1), transform 0.13s cubic-bezier(0.55, 0.055, 0.675, 0.19); 
     }
-    .hamburger--collapse.is-active .hamburger-inner {
+    &.hamburger--collapse.is-active .hamburger-inner {
       transform: translate3d(0, -10px, 0) rotate(-45deg);
       transition-delay: 0.22s;
       transition-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1); 
     }
-    .hamburger--collapse.is-active .hamburger-inner::after {
+    &.hamburger--collapse.is-active .hamburger-inner::after {
       top: 0;
       opacity: 0;
       transition: top 0.2s cubic-bezier(0.33333, 0, 0.66667, 0.33333), opacity 0.1s 0.22s linear; 
     }
-    .hamburger--collapse.is-active .hamburger-inner::before {
+    &.hamburger--collapse.is-active .hamburger-inner::before {
       top: 0;
       transform: rotate(-90deg);
       transition: top 0.1s 0.16s cubic-bezier(0.33333, 0, 0.66667, 0.33333), transform 0.13s 0.25s cubic-bezier(0.215, 0.61, 0.355, 1); 
