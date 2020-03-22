d9f65284eca2e88559fc613286c0910898752dd2
Adam Edwards <aedwards1987@gmail.com>
2020-03-22 16:26:52 +0000

    trying a method to access window on render

--- sw-heating-gatsby/src/helpers/getScrollPosition.js
+++ sw-heating-gatsby/src/helpers/getScrollPosition.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from "react"
+// import React, { useState, useEffect } from "react"
 
 
-const getScrollPosition = ({ scrollUp, currentPosition }) => {
-  // Nav Bar shows on scroll up and vanishes on scroll down
-  // const [currentPositio, setCurrentPositio] = useState(window.pageYOffset)
-  // const [scrollU, setScrollU] = useState(false)
-  useEffect(() => {
-    const handleScroll = () => {
-      const newPosition = window.pageYOffset
-      setScrollUp(currentPosition === 0 ? false : currentPosition > newPosition);
-      setCurrentPosition(newPosition)
-    };
-    window.addEventListener("scroll", handleScroll);
-    return (() => {
-      window.removeEventListener("scroll", handleScroll);
-    })
-  })
-}
+// const getScrollPosition = ({ scrollUp, currentPosition }) => {
+//   // Nav Bar shows on scroll up and vanishes on scroll down
+//   // const [currentPositio, setCurrentPositio] = useState(window.pageYOffset)
+//   // const [scrollU, setScrollU] = useState(false)
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const newPosition = window.pageYOffset
+//       setScrollUp(currentPosition === 0 ? false : currentPosition > newPosition);
+//       setCurrentPosition(newPosition)
+//     };
+//     window.addEventListener("scroll", handleScroll);
+//     return (() => {
+//       window.removeEventListener("scroll", handleScroll);
+//     })
+//   })
+// }
 
-export default getScrollPosition
+// export default getScrollPosition
--- sw-heating-gatsby/src/hooks/useScrollPosition.js
+++ sw-heating-gatsby/src/hooks/useScrollPosition.js
@@ -2,19 +2,24 @@ import { useState, useEffect } from 'react';
 
 
 export default function useScrollPosition(on) {
-  const [currentPosition, setCurrentPosition] = useState(window.pageYOffset)
+  const [currentPosition, setCurrentPosition] = useState('')
   const [scrollUp, setScrollUp] = useState(false)
 
- 
+  useEffect(() => {
+    setCurrentPosition(window.pageYOffset)
+    // return () => {
+    //   cleanup
+    // }
+  }, [currentPosition])
 
   // Listens for the when the page is scrolled up
   useEffect(() => {
-      const handleScroll = () => {
-          const newPosition = window.pageYOffset
-          setScrollUp(currentPosition === 0 ? false : currentPosition > newPosition);
-          setCurrentPosition(newPosition)
-      };
-      window.addEventListener("scroll", handleScroll);
+    const handleScroll = () => {
+      const newPosition = window.pageYOffset
+      setScrollUp(currentPosition === 0 ? false : currentPosition > newPosition);
+      setCurrentPosition(newPosition)
+    };
+    window.addEventListener("scroll", handleScroll);
     return (() => {
       window.removeEventListener("scroll", handleScroll);
     })
