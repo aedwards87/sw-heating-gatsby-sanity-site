56dd68c33f88415f9c9ac99be9cd514514b930aa
Adam Edwards <aedwards1987@gmail.com>
2020-08-16 18:08:26 +0100

    added project env projectid with netlify

--- sw-heating-gatsby/gatsby-config.js
+++ sw-heating-gatsby/gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-sanity',
       options: {
-        projectId: 'z4ilqqhg',
+        projectId: SANITY_PROJECT_ID,
         dataset: 'production',
         overlayDrafts: true,
         watchMode: true,
